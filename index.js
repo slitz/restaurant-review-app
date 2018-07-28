cca96467d52f9ea829b582e63203fd22c3374955
Scott Litz <scott.litz@gmail.com>
2018-07-28 14:54:20 -0700

    Added cache handling.

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+var staticCacheName = 'restaurant-reviews-static-v2';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache){
+      return cache.addAll([
+        '/',
+        'css/styles.css',
+        'data/restaurants.json',
+        'img/',
+        'js/'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event){
+    event.waitUntil(
+      caches.keys().then(function(cacheNames){
+        return Promise.all(
+          cacheNames.filter(function(cacheName){
+            return cacheName.startsWith('restaurant-reviews') &&
+              cacheName != staticCacheName;
+          }).map(function(cacheName){
+            return cache.delete(cacheName);
+          })
+        );
+      })
+    );
+});
+
 self.addEventListener('fetch', function(event) {
-  console.log(event.request);
-})
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      if(response) return response;
+      return fetch(event.request);
+    })
+  );
+});
