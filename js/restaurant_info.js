52c003c5e1f37757771a8e81526031f6eb390f2e
Scott Litz <scott.litz@gmail.com>
2018-08-01 19:36:32 -0700

    Made responsive design adjustments to restaurant details page.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -150,18 +150,22 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
+  name.setAttribute('class', 'reviewer-name');
   name.innerHTML = review.name;
   li.appendChild(name);
 
   const date = document.createElement('p');
+  date.setAttribute('class', 'review-date');
   date.innerHTML = review.date;
   li.appendChild(date);
 
   const rating = document.createElement('p');
+  rating.setAttribute('class', 'review-rating');
   rating.innerHTML = `Rating: ${review.rating}`;
   li.appendChild(rating);
 
   const comments = document.createElement('p');
+  comments.setAttribute('class', 'review-comments');
   comments.innerHTML = review.comments;
   li.appendChild(comments);
 
